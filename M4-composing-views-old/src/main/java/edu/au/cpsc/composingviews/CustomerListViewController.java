462a507c0838ebd3e4f37836760650a3e70a07b4
C. David Shaffer <cdshaffer@acm.org>
2023-08-17 15:07:19 -0400

    Misc additions

---
+++ M4-composing-views-old/src/main/java/edu/au/cpsc/composingviews/CustomerListViewController.java
@@ -0,0 +1,50 @@
+package edu.au.cpsc.composingviews;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+
+public class CustomerListViewController {
+
+  @FXML
+  private ListView<Customer> personList;
+
+  @FXML
+  private CustomerDetailEditorController customerDetailEditorController;
+
+  public void initialize() {
+    personList.setItems(FXCollections.observableList(Customer.demoCustomers()));
+    personList.getSelectionModel().selectedItemProperty()
+        .addListener(event -> listSelectionChanged());
+    personList.setCellFactory(theListView -> new CustomerNameCell());
+  }
+
+  private void listSelectionChanged() {
+    // show bug when noting selected
+    Customer selectedCustomer = personList.getSelectionModel().getSelectedItem();
+    customerDetailEditorController.showCustomer(selectedCustomer);
+  }
+
+  static class CustomerNameCell extends ListCell<Customer> {
+
+    @Override
+    protected void updateItem(Customer item, boolean empty) {
+      // You must do this!!!
+      super.updateItem(item, empty);
+
+      if (empty || item == null) {
+        setText(null);
+        setGraphic(null);
+      } else {
+        setText(item.getName());
+        if (item.getEmail().endsWith(".com"))
+          setTextFill(Color.RED);
+      }
+    }
+  }
+}
---
+++ M4-composing-views-old/src/main/java/edu/au/cpsc/composingviews/CustomerTableViewController.java
@@ -0,0 +1,41 @@
+package edu.au.cpsc.composingviews;
+
+
+import javafx.collections.FXCollections;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class CustomerTableViewController  {
+
+  @FXML
+  private TableView<Customer> customerTableView;
+
+  @FXML
+  private TableColumn<Customer, String> nameColumn, emailColumn, phoneNumberColumn;
+
+  @FXML
+  private CustomerDetailEditorController customerDetailEditorController;
+
+
+  public void initialize() {
+    nameColumn.setCellValueFactory(new PropertyValueFactory<Customer,String>("name"));
+    emailColumn.setCellValueFactory(new PropertyValueFactory<Customer,String>("email"));
+    phoneNumberColumn.setCellValueFactory(new PropertyValueFactory<Customer,String>("phoneNumber"));
+    SortedList<Customer> sortedList = new SortedList<>(
+        FXCollections.observableList(Customer.demoCustomers()));
+    customerTableView.setItems(sortedList);
+    sortedList.comparatorProperty().bind(customerTableView.comparatorProperty());
+    customerTableView.getSelectionModel().selectedItemProperty().addListener(c -> tableSelectionChanged());
+  }
+
+  private void tableSelectionChanged() {
+    Customer selectedCustomer = customerTableView.getSelectionModel().getSelectedItem();
+    customerDetailEditorController.showCustomer(selectedCustomer);
+  }
+}
+
